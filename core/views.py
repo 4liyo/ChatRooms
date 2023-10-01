3f753a6b57a07d93013ababfb6d3831d3de44f6b
alii3111 <aliimohamud185@gmail.com>
2023-10-01 17:58:28 +0300

    add messages

--- core/views.py
+++ core/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
 
 def signUp(request):
-    return render(request, "core/signup.html")
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            fname = form.cleaned_data['username']
+            login(request, user)
+            messages.success(request, f'Successfully added {fname}')
+            return redirect('home')
+
+  
+    form = SignUpForm()
+    return render(request, "core/signup.html", {'form':form})
 
 def logIn(request):
     return render(request, "core/login.html")
