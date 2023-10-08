398ae566140ddb313219108b587c7a656bf611b0
alii3111 <aliimohamud185@gmail.com>
2023-10-08 19:29:45 +0300

    add log in

--- core/views.py
+++ core/views.py
@@ -15,13 +15,12 @@ def signUp(request):
         if form.is_valid():
             user = form.save()
             fname = form.cleaned_data['username']
-            login(request, user)
+            # login(request, user)
             messages.success(request, f'Successfully added {fname}')
             return redirect('login')
 
     form = SignUpForm(request.POST)
     return render(request, "core/signup.html", {'form':form})
 
-def logIn(request):
-    
+def logIn(request):    
     return render(request, "core/login.html")
